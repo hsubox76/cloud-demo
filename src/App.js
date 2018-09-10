6d90ebc4032e65ed0354ebb1b467dee3ad4ff2ca
Christina Holland <hsubox@gmail.com>
2018-09-10 19:20:19 +0000

    Firebase add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 const enemies = [
   'cat',
@@ -11,16 +13,46 @@ const enemies = [
 class App extends Component {
   constructor() {
     super();
+    
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBsyFvxigPX-W5XebG99dZ0_J0UR7--x2w",
+      authDomain: "test1-d075b.firebaseapp.com",
+      databaseURL: "https://test1-d075b.firebaseio.com",
+      projectId: "test1-d075b",
+      storageBucket: "test1-d075b.appspot.com",
+      messagingSenderId: "766394707590"
+    };
+    
+    firebase.initializeApp(config);
+    
+    // Initialize Cloud Firestore through Firebase
+    this.db = firebase.firestore();
+    
+    // Disable deprecated features
+    this.db.settings({
+      timestampsInSnapshots: true
+    });
+    
     this.state = {
-      enemies: enemies,
+      enemies: [],
       userInput: ''
     }
   }
   addEnemy = (enemyToAdd) => {
     this.setState({
-      enemies: this.state.enemies.concat(enemyToAdd),
+      // enemies: this.state.enemies.concat(enemyToAdd),
       userInput: ''
     });
+    
+    this.db.collection("enemies")
+      .add({ name: enemyToAdd })
+      .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+          console.error("Error adding document: ", error);
+      });
   }
   removeEnemy = (enemyToRemove) => {
     this.setState({
@@ -36,16 +68,16 @@ class App extends Component {
         </header>
         <div className="add-enemy-form">
           <input value={this.state.userInput} onChange={(e) => this.setState({ userInput: e.target.value })} />
-          <button onClick={() => this.setState({ enemies: this.state.enemies.concat(this.state.userInput)})}>add enemy</button>
+          <button onClick={() => this.addEnemy(this.state.userInput)}>add enemy</button>
         </div>
-        <p className="enemies-list">
+        <div className="enemies-list">
           {this.state.enemies.map(enemy => (
             <div key={enemy}>
               <input type="checkbox" onClick={() => this.removeEnemy(enemy)} />
               <label>{enemy}</label>
             </div>
           ))}
-        </p>
+        </div>
       </div>
     );
   }
