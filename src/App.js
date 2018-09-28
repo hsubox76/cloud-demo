afb851e2bded1c3844d3e5ce175f01c269bfd298
Christina Holland <hsubox@gmail.com>
2018-09-28 20:23:21 +0000

    Remove auth which goes in step6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
 import 'firebase/firestore';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ]
-};
 
 class App extends Component {
   constructor() {
     super();
     
     // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyBsyFvxigPX-W5XebG99dZ0_J0UR7--x2w",
-      authDomain: "test1-d075b.firebaseapp.com",
-      databaseURL: "https://test1-d075b.firebaseio.com",
-      projectId: "test1-d075b",
-      storageBucket: "test1-d075b.appspot.com",
-      messagingSenderId: "766394707590"
+    const projectId = process.env.REACT_APP_DEMO_PROJECT_ID;
+    
+    // Pull secrets from env so it doesn't get saved in your repo
+    const config = {
+      apiKey: process.env.REACT_APP_DEMO_API_KEY,
+      authDomain: projectId + ".firebaseapp.com",
+      databaseURL: "https://" + projectId + ".firebaseio.com",
+      projectId: projectId,
+      storageBucket: projectId + ".appspot.com"
     };
     
     firebase.initializeApp(config);
@@ -54,9 +42,6 @@ class App extends Component {
           enemies: querySnapshot.docs.map(enemySnapshot => enemySnapshot.data().name)
         });
       });
-    firebase.auth().onAuthStateChanged(
-      (user) => this.setState({ user })
-    );
   }
   addEnemy = (enemyToAdd) => {
     this.setState({
@@ -81,15 +66,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {!this.state.user && <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>}
-          {this.state.user && <a onClick={() => firebase.auth().signOut()}>Sign-out</a>}
         </header>
-        {this.state.user && this.state.user.uid === 'LH2kvfTu3LXMSecOhIp9YmHAoez1' && (
-          <div className="add-enemy-form">
-            <input value={this.state.userInput} onChange={(e) => this.setState({ userInput: e.target.value })} />
-            <button onClick={() => this.addEnemy(this.state.userInput)}>add enemy</button>
-          </div>
-        )}
+        <div className="add-enemy-form">
+          <input value={this.state.userInput} onChange={(e) => this.setState({ userInput: e.target.value })} />
+          <button onClick={() => this.addEnemy(this.state.userInput)}>add enemy</button>
+        </div>
         <div className="enemies-list">
           {this.state.enemies.map(enemy => (
             <div key={enemy}>
