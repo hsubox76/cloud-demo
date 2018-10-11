f604e91467e6b6ca5b5bf50c945a9a270686d152
Christina Holland <hsubox@gmail.com>
2018-10-11 22:42:18 +0000

    Shrink header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const enemies = [
@@ -32,8 +31,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          this is where the header was
         </header>
         {/* Form for adding a new enemy */}
         <div className="add-enemy-form">
