73c29ea77ef230f77b1a6e560d94e2e56c318f05
Christina Holland <hsubox@gmail.com>
2018-10-11 22:36:29 +0000

    Reduce header size

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // List of enemies - hardcoded
@@ -24,8 +23,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="enemies-list">
           {/* checklist */}
