2deec6d1e15e536f1513282b0575a28502e81159
Christina Holland <hsubox@gmail.com>
2018-10-19 18:51:22 +0000

    Update step4 with new way and comments

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import firebase from 'firebase';
 import 'firebase/firestore';
-import { config } from './config';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      enemies: [],
+    };
+    this.inputRef = React.createRef();
+    
+    // ************* STEP 4 ****************
+    // Insert Firebase config here!
+    // ************* STEP 4 ****************
+    const config = {};
     
     firebase.initializeApp(config);
-    
-    // Initialize Cloud Firestore through Firebase
-    this.db = firebase.firestore();
-    
-    // Disable deprecated features
-    this.db.settings({
-      timestampsInSnapshots: true
-    });
-    
-    this.state = {
-      enemies: [], // still inits empty
-      userInput: ''
-    }
+    this.firestore = firebase.firestore();
   }
   componentDidMount = () => {
-    // Read enemies/ collection from firebase.
-    this.db.collection("enemies")
-      // "onSnapshot" sets a listener that will be called when collection
-      // changes
+    // ************* STEP 4 ****************
+    // Read enemies collection from Firebase
+    // ************* STEP 4 ****************
+    this.firestore.collection("enemies")
+      // ************* STEP 4 ****************
+      // "onSnapshot" sets a listener that is
+      // called whenever collection changes
+      // ************* STEP 4 ****************
       .onSnapshot((querySnapshot) => {
         this.setState({
           enemies: querySnapshot.docs.map(enemySnapshot => enemySnapshot.data().name)
@@ -35,21 +35,11 @@ class App extends Component {
       });
   }
   addEnemy = (enemyToAdd) => {
-    this.setState({
-      userInput: ''
-    });
-    
-    this.db.collection("enemies")
-      .add({ name: enemyToAdd })
-      .then(function(docRef) {
-          console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function(error) {
-          console.error("Error adding document: ", error);
-      });
+    this.firestore.collection("enemies")
+      .add({ name: this.inputRef.current.value })
+      .then(() => this.inputRef.current.value = '');
   }
   removeEnemy = (enemyToRemove) => {
-    // Removal does NOT write to backend - client side only
     this.setState({
       enemies: this.state.enemies.filter(enemy => enemy !== enemyToRemove)
     });
@@ -58,18 +48,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          reading from db, no auth roles
+          Writing to AND reading from DB
         </header>
         <div className="add-enemy-form">
-          <input value={this.state.userInput} onChange={(e) => this.setState({ userInput: e.target.value })} />
-          <button onClick={() => this.addEnemy(this.state.userInput)}>add enemy</button>
+          <input ref={this.inputRef} />
+          <button onClick={this.addEnemy}>
+            add enemy
+          </button>
         </div>
         <div className="enemies-list">
           {this.state.enemies.map(enemy => (
-            <div key={enemy}>
-              <input type="checkbox" onClick={() => this.removeEnemy(enemy)} />
-              <label>{enemy}</label>
-            </div>
+            <button onClick={() => this.removeEnemy(enemy)} key={enemy}>
+              {enemy}
+            </button>
           ))}
         </div>
       </div>
--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const config = {
-	// firebase config goes here
-};
