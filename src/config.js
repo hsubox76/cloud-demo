50b6ff02f5fc6746d2fcd835253e70b873cb9cb3
Christina Holland <hsubox@gmail.com>
2018-10-19 01:18:35 +0000

    Fix config call

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
 export const config = {
-	// firebase config goes here
-};
+    apiKey: "AIzaSyDzUMsNcfLJkn6iGuOXEaDfG_gshXSy41o",
+    authDomain: "durian-pizza.firebaseapp.com",
+    databaseURL: "https://durian-pizza.firebaseio.com",
+    projectId: "durian-pizza",
+    storageBucket: "durian-pizza.appspot.com",
+    messagingSenderId: "720445715595"
+  };
